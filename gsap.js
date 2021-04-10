c8261f6f96520b47ef2e0853ed612859a0970aee
Jason Nutt <jnutt367@gmail.com>
2021-04-10 19:26:20 -0400

    commit new ease effect

--- gsap.js
+++ gsap.js
@@ -1,4 +1,4 @@
-gsap.to(".a", { x: 400, rotation: 360, duration: 3 });
+gsap.to(".a", { x: 200, rotation: 360, duration: 3 });
 gsap.to(".b", { duration: 2, borderRadius: "50%" });
 gsap.to(".c", { duration: 3, backgroundColor: "black", x: 120, y: -120, textAlign: "center", fontFamily: "cursive", textContent: "You can even target text inside objects", height: "300", width: "300", fontSize: "50" })
 gsap.from(".h1", { duration: 2, scale: "1.5", ease: "elastic", rotation: 45, repeat: "-1" });
@@ -17,4 +17,7 @@ gsap.to(".h1", { color: "black" });
 
 gsap.fromTo(".fish", { duration: 2, x: 200, y: 200, opacity: 0 }, { duration: 3, x: 120, y: -550, opacity: 1 });
 
-gsap.fromTo(".svg", { duration: 2, x: 2, y: 200, opacity: 0 }, { duration: 3, x: 5, y: -50, rotate: 45, backgroundColor: "purple", margin: "50", height: "100", width: "100", opacity: .9 })
+gsap.fromTo(".svg", { duration: 2, x: 2, y: 200, opacity: 0 }, { duration: 3, x: 5, y: -50, rotate: 45, backgroundColor: "purple", margin: "50", height: "100", width: "100", opacity: .9 });
+
+gsap.to(
+    ".new", { duration: 2.5, ease: "bounce.inOut", y: -1300, color: "black" });
