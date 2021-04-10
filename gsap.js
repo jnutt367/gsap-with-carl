c1b3cd3d069c2afc0ede43048422eb301676e780
Jason Nutt <jnutt367@gmail.com>
2021-04-10 16:34:45 -0400

    commit my next svg animation

--- gsap.js
+++ gsap.js
@@ -1,6 +1,6 @@
 gsap.to(".a", { x: 400, rotation: 360, duration: 3 });
 gsap.to(".b", { duration: 2, borderRadius: "50%" });
-gsap.to(".c", { duration: 3, backgroundColor: "black", x: 120, y: -120, textAlign: "center", textContent: "This works!", height: "300", width: "300", fontSize: "50" })
+gsap.to(".c", { duration: 3, backgroundColor: "black", x: 120, y: -120, textAlign: "center", fontFamily: "cursive", textContent: "You can even target text inside objects", height: "300", width: "300", fontSize: "50" })
 gsap.from(".h1", { duration: 2, scale: "1.5", ease: "elastic", rotation: 45 });
 
 gsap.from(".h2", { duration: 1.5, scale: "2", ease: "back" });
@@ -15,4 +15,6 @@ gsap.to(".h4", { duration: 3, scale: 1.1, color: "black", padding: 20 });
 
 gsap.to(".h1", { color: "black" });
 
-gsap.fromTo(".fish", { duration: 2, x: 200, y: 200, opacity: 0 }, { duration: 3, x: 120, y: -600, opacity: 1 })
+gsap.fromTo(".fish", { duration: 2, x: 200, y: 200, opacity: 0 }, { duration: 3, x: 120, y: -600, opacity: 1 });
+
+gsap.fromTo(".svg", { duration: 2, x: 2, y: 200, opacity: 0 }, { duration: 3, x: 5, y: -700, rotate: 45, opacity: .9 })
