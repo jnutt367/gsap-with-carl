63cf082936bf5e304d7d45a129e0ef5f7efc92fd
Jason Nutt <jnutt367@gmail.com>
2021-04-10 15:43:16 -0400

    commit final draft for first day teachings

--- gsap.js
+++ gsap.js
@@ -7,10 +7,12 @@ gsap.from(".h2", { duration: 1.5, scale: "2", ease: "back" });
 
 gsap.to(".h3", { duration: 5, x: 50, textAlign: "left", paddingRight: 50, });
 
-gsap.from(".ginger", { duration: 1.5, x: 300, scale: "3", ease: "back" });
-
+gsap.from(".ginger", { duration: 1.5, fontSize: "100", x: 300, scale: "5", ease: "back" });
+gsap.to(".ginger", { duration: 2.5, scaleX: "1.1", scaleY: "2", color: "red", paddingBottom: 20 })
 gsap.from("body", { duration: 2.5, backgroundColor: "gold" });
 
-gsap.to(".h4", { duration: 3, scale: 1.1, color: "black", padding: 100 });
+gsap.to(".h4", { duration: 3, scale: 1.1, color: "black", padding: 20 });
 
 gsap.to(".h1", { color: "black" });
+
+gsap.fromTo(".fish", { duration: 2, x: 200, y: 200, opacity: 0 }, { duration: 3, x: 120, y: -600, opacity: 1 })
